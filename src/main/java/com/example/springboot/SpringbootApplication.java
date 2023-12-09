4ec39cf696a17112171fb0fb9c286bff94dca804
Nande14 <nande.nave@gmail.com>
2023-12-08 22:49:35 -0300

    Criação da base da API

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,7 +1,10 @@
 package com.example.springboot;
 
+import com.example.springboot.models.ProductModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringbootApplication {
